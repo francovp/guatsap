fc69a6280d10b1b5b748ec4c0daf5e7bb72d3ca3
Niia <shooting_star-.-@hotmail.com>
2016-06-08 15:27:29 -0400

    ChatClientAgent - Español

--- src/chat/client/agent/ChatClientAgent.java
+++ src/chat/client/agent/ChatClientAgent.java
@@ -1,26 +1,3 @@
-/*****************************************************************
-JADE - Java Agent DEvelopment Framework is a framework to develop 
-multi-agent systems in compliance with the FIPA specifications.
-Copyright (C) 2000 CSELT S.p.A. 
-
-GNU Lesser General Public License
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation, 
-version 2.1 of the License. 
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the
-Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA  02111-1307, USA.
- *****************************************************************/
-
 package chat.client.agent;
 
 import jade.content.ContentManager;
@@ -42,26 +19,22 @@ import jade.util.leap.Iterator;
 import jade.util.leap.Set;
 import jade.util.leap.SortedSetImpl;
 import chat.client.ChatGui;
-/*#MIDP_INCLUDE_BEGIN
-import chat.client.MIDPChatGui;
-#MIDP_INCLUDE_END*/
-//#MIDP_EXCLUDE_BEGIN
+
 import chat.client.AWTChatGui;
-//#MIDP_EXCLUDE_END
+
 import chat.ontology.ChatOntology;
 
 /**
- * This agent implements the logic of the chat client running on the user
- * terminal. User interactions are handled by the ChatGui in a
- * terminal-dependent way. The ChatClientAgent performs 3 types of behaviours: -
- * ParticipantsManager. A CyclicBehaviour that keeps the list of participants up
- * to date on the basis of the information received from the ChatManagerAgent.
- * This behaviour is also in charge of subscribing as a participant to the
- * ChatManagerAgent. - ChatListener. A CyclicBehaviour that handles messages
- * from other chat participants. - ChatSpeaker. A OneShotBehaviour that sends a
- * message conveying a sentence written by the user to other chat participants.
- * 
- * @author Giovanni Caire - TILAB
+ * Este agente implementa la logica del chat cliente que esta ejecutandose en el terminal del usuario.
+ * Las interacciones del usuario están manejadas por el "ChatGui" de una manera dependiente. Esta clase
+ * utiliza 3 comportamientos:
+ * 	- ParticipantsManager. Un CyclicBehaviour que mantiene la lista de participantes actualizada
+ * 	basada en la información recibida desde ChatManagerAgent. También está a cargo de suscribir a los
+ * 	participantes al ChatManagerAgent.
+ * 	- ChatListener. Un CyclicBehaviour que maneja los mensajes desde los chat de los otros participantes.
+ * 	- ChatSpeaker. Un OneShotBehaviour que envía un mensaje transformado en una sentencia escrita por el
+ * 	usuario a los otros participantes del chat
+ *
  */
 public class ChatClientAgent extends Agent {
 	private static final long serialVersionUID = 1594371294421614291L;
@@ -78,28 +51,22 @@ public class ChatClientAgent extends Agent {
 	private ACLMessage spokenMsg;
 
 	protected void setup() {
-		// Register language and ontology
+		// Registramos el lenguaje y la ontología
 		ContentManager cm = getContentManager();
 		cm.registerLanguage(codec);
 		cm.registerOntology(onto);
 		cm.setValidationMode(false);
 
-		// Add initial behaviours
+		// Agregamos comportamientos iniciales
 		addBehaviour(new ParticipantsManager(this));
 		addBehaviour(new ChatListener(this));
 
-		// Initialize the message used to convey spoken sentences
+		// Inicializamos el mensaje usado para transmitir las sentencias de los usuarios
 		spokenMsg = new ACLMessage(ACLMessage.INFORM);
 		spokenMsg.setConversationId(CHAT_ID);
 
-		// Activate the GUI
-		//#MIDP_EXCLUDE_BEGIN
+		// Activamos el GUI
 		myGui = new AWTChatGui(this);
-		//#MIDP_EXCLUDE_END
-
-		/*#MIDP_INCLUDE_BEGIN
-		myGui = new MIDPChatGui(this);
-		#MIDP_INCLUDE_END*/
 	}
 
 	protected void takeDown() {
@@ -117,9 +84,9 @@ public class ChatClientAgent extends Agent {
 	}
 	
 	/**
-	 * Inner class ParticipantsManager. This behaviour registers as a chat
-	 * participant and keeps the list of participants up to date by managing the
-	 * information received from the ChatManager agent.
+	 * Clase interna ParticipantsManager. Este comportamiento registra a los participantes del chat
+	 * y mantiene una lista de participantes actualizada para gestionar la información recibida por
+	 * el agente ChatManager.
 	 */
 	class ParticipantsManager extends CyclicBehaviour {
 		private static final long serialVersionUID = -4845730529175649756L;
@@ -130,55 +97,49 @@ public class ChatClientAgent extends Agent {
 		}
 
 		public void onStart() {
-			// Subscribe as a chat participant to the ChatManager agent
+			// Suscribe a un participante del chat al agente ChatManager
 			ACLMessage subscription = new ACLMessage(ACLMessage.SUBSCRIBE);
 			subscription.setLanguage(codec.getName());
 			subscription.setOntology(onto.getName());
 			String convId = "C-" + myAgent.getLocalName();
 			subscription.setConversationId(convId);
-			subscription
-					.addReceiver(new AID(CHAT_MANAGER_NAME, AID.ISLOCALNAME));
+			subscription.addReceiver(new AID(CHAT_MANAGER_NAME, AID.ISLOCALNAME));
 			myAgent.send(subscription);
-			// Initialize the template used to receive notifications
-			// from the ChatManagerAgent
+			// Inicializamos la plantilla usada para recibir notificaciones desde el ChatManagerAgent
 			template = MessageTemplate.MatchConversationId(convId);
 		}
 
 		public void action() {
-			// Receives information about people joining and leaving
-			// the chat from the ChatManager agent
+			// Recibe información sobre la gente que entra y sale del chat
 			ACLMessage msg = myAgent.receive(template);
 			if (msg != null) {
 				if (msg.getPerformative() == ACLMessage.INFORM) {
 					try {
-						AbsPredicate p = (AbsPredicate) myAgent
-							.getContentManager().extractAbsContent(msg);
-						if (p.getTypeName().equals(ChatOntology.JOINED)) {
-							// Get new participants, add them to the list of
-							// participants and notify the gui
-							AbsAggregate agg = (AbsAggregate) p
-									.getAbsTerm(ChatOntology.JOINED_WHO);
+						AbsPredicate p = (AbsPredicate) myAgent.getContentManager().extractAbsContent(msg);
+						// Ve si entra un nuevo participante comparando su ontologia
+						if (p.getTypeName().equals(ChatOntology.JOINED)) { 
+							// Obtiene nuevos particpantes, agregandolos a la lista y 
+							// notificando a la GUI
+							AbsAggregate agg = (AbsAggregate) p.getAbsTerm(ChatOntology.JOINED_WHO);
 							if (agg != null) {
 								Iterator it = agg.iterator();
 								while (it.hasNext()) {
 									AbsConcept c = (AbsConcept) it.next();
-									participants.add(BasicOntology
-											.getInstance().toObject(c));
+									participants.add(BasicOntology.getInstance().toObject(c));
 								}
 							}
 							notifyParticipantsChanged();
 						}
+						// Ve si sale un nuevo participante comparando su ontologia
 						if (p.getTypeName().equals(ChatOntology.LEFT)) {
-							// Get old participants, remove them from the list
-							// of participants and notify the gui
-							AbsAggregate agg = (AbsAggregate) p
-									.getAbsTerm(ChatOntology.JOINED_WHO);
+							// Rescata a los antiguos participantes, los remueve de la lista
+							// y notifica a la GUI
+							AbsAggregate agg = (AbsAggregate) p.getAbsTerm(ChatOntology.JOINED_WHO);
 							if (agg != null) {
 								Iterator it = agg.iterator();
 								while (it.hasNext()) {
 									AbsConcept c = (AbsConcept) it.next();
-									participants.remove(BasicOntology
-											.getInstance().toObject(c));
+									participants.remove(BasicOntology.getInstance().toObject(c));
 								}
 							}
 							notifyParticipantsChanged();
@@ -194,17 +155,16 @@ public class ChatClientAgent extends Agent {
 				block();
 			}
 		}
-	} // END of inner class ParticipantsManager
+	}
 
 	/**
-	 * Inner class ChatListener. This behaviour registers as a chat participant
-	 * and keeps the list of participants up to date by managing the information
-	 * received from the ChatManager agent.
+	 * Clase interna ChatListener. Este comportamiento registra a los participantes del chat
+	 * y mantiene una lista de participantes actualizada para gestionar la información recibida por
+	 * el agente ChatManager.
 	 */
 	class ChatListener extends CyclicBehaviour {
 		private static final long serialVersionUID = 741233963737842521L;
-		private MessageTemplate template = MessageTemplate
-				.MatchConversationId(CHAT_ID);
+		private MessageTemplate template = MessageTemplate.MatchConversationId(CHAT_ID);
 
 		ChatListener(Agent a) {
 			super(a);
@@ -214,8 +174,7 @@ public class ChatClientAgent extends Agent {
 			ACLMessage msg = myAgent.receive(template);
 			if (msg != null) {
 				if (msg.getPerformative() == ACLMessage.INFORM) {
-					notifySpoken(msg.getSender().getLocalName(),
-							msg.getContent());
+					notifySpoken(msg.getSender().getLocalName(), msg.getContent());
 				} else {
 					handleUnexpected(msg);
 				}
@@ -223,11 +182,10 @@ public class ChatClientAgent extends Agent {
 				block();
 			}
 		}
-	} // END of inner class ChatListener
+	}
 
 	/**
-	 * Inner class ChatSpeaker. INFORMs other participants about a spoken
-	 * sentence
+	 * Clase interna ChatSpeaker. Informa a otros participantes que alguien habló
 	 */
 	private class ChatSpeaker extends OneShotBehaviour {
 		private static final long serialVersionUID = -1426033904935339194L;
@@ -248,14 +206,13 @@ public class ChatClientAgent extends Agent {
 			notifySpoken(myAgent.getLocalName(), sentence);
 			send(spokenMsg);
 		}
-	} // END of inner class ChatSpeaker
+	}
 
 	// ///////////////////////////////////////
-	// Methods called by the interface
+	// Métodos llamados por la interfaz
 	// ///////////////////////////////////////
 	public void handleSpoken(String s) {
-		// Add a ChatSpeaker behaviour that INFORMs all participants about
-		// the spoken sentence
+		// Agregar un comportamiento a ChatSpeaker que informa a todos los participantes que alguien habló
 		addBehaviour(new ChatSpeaker(this, s));
 	}
 	
@@ -271,13 +228,12 @@ public class ChatClientAgent extends Agent {
 	}
 
 	// ///////////////////////////////////////
-	// Private utility method
+	// Método de uso privado
 	// ///////////////////////////////////////
 	private void handleUnexpected(ACLMessage msg) {
 		if (logger.isLoggable(Logger.WARNING)) {
-			logger.log(Logger.WARNING, "Unexpected message received from "
-					+ msg.getSender().getName());
-			logger.log(Logger.WARNING, "Content is: " + msg.getContent());
+			logger.log(Logger.WARNING, "Mensaje inesperado recibido desde " + msg.getSender().getName());
+			logger.log(Logger.WARNING, "El contenido es el siguiente: " + msg.getContent());
 		}
 	}
 
