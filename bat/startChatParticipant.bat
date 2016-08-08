e8e05f0a2b950b38a032a59bd4247a2c75268c0e
Franco Valerio <franco.gvp@hotmail.com>
2016-08-08 15:08:25 -0400

    Agregados bats y otros

---
+++ bat/startChatParticipant.bat
@@ -0,0 +1,2 @@
+java -cp ..\lib\jade.jar;..\lib\chatOntology.jar;..\lib\chatStandard.jar jade.Boot -container -host 158.251.93.142 -agents pcprueba:chat.client.agent.ChatClientAgent -exitwhenempty true
+pause
